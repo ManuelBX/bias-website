80e0c37425f58be4be099f61bb4d665884314049
Manuel Braje <manuelbraje@outlook.com>
2024-05-28 03:26:26 -0700

    new site

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template("page.html")
+
+@app.route('/search', methods=["POST", "GET"])
+def result():
+    if request.method == "POST":
+        search = request.form["search"]
+        return redirect(url_for("result", search=search))
+    else:
+        return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
